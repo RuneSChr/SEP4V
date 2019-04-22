31544e0718d4817b3203ceb1dc98e692bfc19c78
runeschr <runechr2014@gmail.com>
2019-04-22 12:32:43 +0200

    added interface colorblindnes
    will handle changing of shaders

---
+++ Other Perspective/Assets/Scripts/IColorBlind.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IColorBlind
+{
+    event System.Action<int> OnShadesChanged;
+    void ChangeShades(int shadeIndex);
+	
+}
