62513b35ea9c3ced8cac3553d39e2554eed84353
PeipeiSu <266128@via.dk>
2019-04-29 15:59:25 +0200

    changes in lesson

--- Other Perspective/Assets/Scripts/OpenTips.cs
+++ Other Perspective/Assets/Scripts/OpenTips.cs
@@ -2,18 +2,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Leap.Unity.Interaction;
+
 
 public class OpenTips : MonoBehaviour
 {
     public GameObject tip;
+    private InteractionBehaviour behave;
 
 	// Use this for initialization
 	void Start () {
-	}
+        behave = GetComponent<InteractionBehaviour>();
+        behave.OnGraspBegin += Test;
+    }
 
-    void OnTriggerEnter(Collider other)
+    void Test()
     {
         tip.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        behave.OnGraspBegin -= Test;
     }
 }
 
