532d8be1d783bb172704215a1d8e850b1e407e13
PeipeiSu <266128@via.dk>
2019-05-29 21:17:09 +0200

    6th puzzle improved

---
+++ Other Perspective/Assets/Scripts/Reading Test/ReadingTestWinTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadingTestWinTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("MainCamera"))
+        //go to next puzzle
+        Debug.Log("Win trigger enter!");
+    }
+}
