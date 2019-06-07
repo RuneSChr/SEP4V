e13272f80d47947b7375671137e2d50c8946b02b
Unknown <baqueta.dani@gmail.com>
2019-06-07 12:44:23 +0200

    Folder with drag the key puzzle added :)

---
+++ Daniela/SEP4V-Colorblindnes-with-gitIgnore/Other Perspective/Assets/Scripts/DragTheKey/ResetSphere.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetSphere : MonoBehaviour {
+
+    private Renderer rend;
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        rend.material.shader = Shader.Find("_Color");
+        rend.material.SetColor("_Color", Color.green);
+
+        rend.material.shader = Shader.Find("Specular");
+        rend.material.SetColor("_SpecColor", Color.red);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rend.material.SetColor("_Color", Color.green);
+        KeyCollide.firstCollide = true;
+    }
+}
